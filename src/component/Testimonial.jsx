1da95c2b1b282d11cfcb14c72e902e7c6a249579
Jamil-coder360 <jamil595nhj@gmail.com>
2026-04-12 04:12:18 +0600

    testimonial component fixed

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "./Button";
 import left from "../assets/left.png";
 import right from "../assets/right.png";
 import PortfolioCard from "./Portfolio";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Mousewheel } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+
+
+import "swiper/css";
+// import "swiper/css/pagination";
 const Testimonial = () => {
+    const swiperRef = useRef(null);
+  
   return (
     <section>
       <div className="container">
@@ -19,20 +28,66 @@ const Testimonial = () => {
               </h1>
             </div>
             <div className="flex gap-3.5 hidden lg:flex">
-              <Button>
+              <Button onClick={() => swiperRef.current?.slidePrev()}>
                 <img src={left} alt="" />
               </Button>
-              <Button>
+              <Button onClick={() => swiperRef.current?.slideNext()}>
                 <img src={right} alt="" />
               </Button>
             </div>
           </div>
-       	<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6">
+          {/* <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6"> */}
+            <Swiper
+           onSwiper={(swiper) => (swiperRef.current = swiper)}
+              slidesPerView={2}
+              // mousewheel={false}
+              spaceBetween={10}
+              loop={true}
+              className="mySwiper"
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1280: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+              }}
+            >
+              <SwiperSlide>
+                <PortfolioCard />
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <PortfolioCard />
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <PortfolioCard />
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <PortfolioCard />
+              </SwiperSlide>
+              
+              <SwiperSlide>
+                <PortfolioCard />
+              </SwiperSlide>
+            </Swiper>
+
+            {/* <PortfolioCard />
 					<PortfolioCard />
-					<PortfolioCard />
-					<PortfolioCard />
-					<PortfolioCard />
-				</div>
+					<PortfolioCard /> */}
+          {/* </div> */}
         </div>
       </div>
     </section>
